5b4d0d12c11d5629d0de29cedd9cad9b184653f6
NguyenDanhLinh <linhtru2001@gmail.com>
2023-07-25 14:00:59 +0700

    create api create user

--- src/app/controllers/users.controller.ts
+++ src/app/controllers/users.controller.ts
@@ -1,21 +1,30 @@
-import { Get, JsonController, Req, Res } from 'routing-controllers'
-import { NextFunction } from 'express'
+import { Get, JsonController, Req, Res, Post, UseBefore, Body } from 'routing-controllers'
 import { BaseController } from './base.controller'
 import { Service } from 'typedi'
-import UserRepository from '@repositories/user.repository'
+import UserServices from '@services/users.service'
+import validationMiddleware from '@middlewares/validation.middleware'
+import { CreateUserDto } from 'dtos/users.dto'
 
 @JsonController('/user')
 @Service()
 export class UsersController extends BaseController {
-  constructor(protected userRepository: UserRepository) {
+  constructor(protected userServices: UserServices) {
     super()
   }
 
   @Get('/list')
-  async getUser(@Req() req: any, @Res() res: any, next: NextFunction) {
-    const findAllUsersData = await this.userRepository.getAll()
+  async getUser(@Res() res: any) {
+    const findAllUsersData = await this.userServices.getAll()
 
     return this.responseSuccess(findAllUsersData, 'Success', res)
+  }
+
+  @Post('/create')
+  @UseBefore(validationMiddleware(CreateUserDto, 'body'))
+  async createUser(@Body() body: CreateUserDto, @Req() req: any, @Res() res: any) {
+    await this.userServices.createUser(body)
+
+    return this.responseSuccess([], 'Success', res)
   }
 }
 
