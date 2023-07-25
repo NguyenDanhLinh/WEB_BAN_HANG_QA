26073b535d0b53b4572a7d61a05bdb13ef3e9300
NguyenDanhLinh <linhtru2001@gmail.com>
2023-07-25 14:35:44 +0700

    create userLogin()

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,7 +1,11 @@
-import { CreateUserInterface } from '@interfaces/user.interface'
+import { CreateUserInterface, UserLoginInterface } from '@interfaces/user.interface'
 import UserRepository from '@repositories/user.repository'
 import { Service } from 'typedi'
 import { hash } from 'bcrypt'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { HttpException } from '@exceptions/http.exception'
+import { env } from '@env'
 
 @Service()
 class UserServices {
@@ -15,6 +19,21 @@ class UserServices {
     body.password = await hash(body.password, 10)
 
     return this.userRepository.create(body)
+  }
+
+  async userLogin(body: UserLoginInterface) {
+    const user = await this.userRepository.findByCondition({
+      where: { userName: body.userName },
+      raw: true,
+    })
+
+    if (!user || !bcrypt.compareSync(body.password, user.password)) {
+      throw new HttpException(400, 'wrong username or password')
+    }
+
+    delete user.password
+
+    return jwt.sign(user, env.auth.jwtSecret)
   }
 }
 
