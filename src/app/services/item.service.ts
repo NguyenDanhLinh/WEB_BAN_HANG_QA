c3f91bc07b9d33a733456af6ea59e2a9c6d77033
NguyenDanhLinh <linhtru2001@gmail.com>
2023-07-26 17:37:26 +0700

    init service items

---
+++ src/app/services/item.service.ts
@@ -0,0 +1,17 @@
+import { CreateUserInterface, UserLoginInterface } from '@interfaces/user.interface'
+import UserRepository from '@repositories/user.repository'
+import { Service } from 'typedi'
+import { hash } from 'bcrypt'
+import bcrypt from 'bcrypt'
+import { HttpException } from '@exceptions/http.exception'
+import { env } from '@env'
+import CronServices from 'vendor-services/cronJob.service'
+import CategoryRepository from '@repositories/category.repository'
+import ItemRepository from '@repositories/item.repository'
+
+@Service()
+class ItemServices {
+  constructor(protected itemRepository: ItemRepository, protected cronServices: CronServices) {}
+}
+
+export default ItemServices
