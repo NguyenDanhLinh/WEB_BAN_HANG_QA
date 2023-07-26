916be0d21f87d51086488b18257635d635aa1fbb
NguyenDanhLinh <linhtru2001@gmail.com>
2023-07-26 10:35:46 +0700

    create service create category

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -7,13 +7,24 @@ import { HttpException } from '@exceptions/http.exception'
 import { env } from '@env'
 import CronServices from 'vendor-services/cronJob.service'
 import CategoryRepository from '@repositories/category.repository'
+import { UploadToFilebaseService } from '@common/services/upload_file.service'
+import { File } from '@interfaces/file.interface'
+import { CreateCategoryInterface } from '@interfaces/category.interface'
 
 @Service()
 class CategoryServices {
   constructor(
     protected categoryRepository: CategoryRepository,
     protected cronServices: CronServices,
+    protected uploadToFilebaseService: UploadToFilebaseService,
   ) {}
+
+  async createCategory(file: File, body: CreateCategoryInterface) {
+    const img = await this.uploadToFilebaseService.uploadFile(file[0])
+    body.img = img
+
+    return this.categoryRepository.create(body)
+  }
 }
 
 export default CategoryServices
