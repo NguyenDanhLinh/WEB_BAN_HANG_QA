010c161f2865420b0f47e0dbac01364a3e574e5f
NguyenDanhLinh <linhtru2001@gmail.com>
2023-07-26 13:48:16 +0700

    create getListCategories() in repository

--- src/app/repositories/category.repository.ts
+++ src/app/repositories/category.repository.ts
@@ -4,6 +4,7 @@ import { BaseRepository } from './base.repository'
 import { ModelContainer } from '@decorators/model.decorator'
 import { CategoryRepositoryInterface } from './interfaces/category.repository.interface'
 import Category from '@models/entities/categories.entity'
+import { WhereOptions } from 'sequelize'
 
 @Service({ global: true })
 class CategoryRepository
@@ -12,6 +13,20 @@ class CategoryRepository
 {
   constructor(@ModelContainer(Category.tableName) Category: ModelCtor<Category>) {
     super(Category)
+  }
+
+  async getListCategories(
+    whereClause: WhereOptions<Category>,
+    offset: number,
+    limit: number,
+    orderBy: any,
+  ): Promise<any> {
+    return this.model.findAndCountAll({
+      where: whereClause,
+      order: orderBy,
+      offset,
+      limit,
+    })
   }
 }
 
