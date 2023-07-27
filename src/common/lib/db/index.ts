885eaabca99891207dacc4a46aec10b62528dc08
NguyenDanhLinh <linhtru2001@gmail.com>
2023-07-27 09:22:15 +0700

    entity voucher

--- src/common/lib/db/index.ts
+++ src/common/lib/db/index.ts
@@ -4,6 +4,7 @@ import DB from '@models/index'
 import User from '@models/entities/users.entity'
 import Category from '@models/entities/categories.entity'
 import Item from '@models/entities/items.entity'
+import Voucher from '@models/entities/voucher.entity'
 
 export function getModelFromTableName(tableName: string): ModelCtor | undefined {
   let item = undefined
@@ -16,6 +17,9 @@ export function getModelFromTableName(tableName: string): ModelCtor | undefined 
       break
     case Item.tableName:
       item = DB.sequelize.model(Item)
+      break
+    case Voucher.tableName:
+      item = DB.sequelize.model(Voucher)
       break
     default:
       item = undefined
