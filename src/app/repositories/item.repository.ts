deeb29a501a5528f8af65037bfd08795b95bb4a5
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-06 14:11:14 +0700

    create repository

--- src/app/repositories/item.repository.ts
+++ src/app/repositories/item.repository.ts
@@ -4,11 +4,26 @@ import { BaseRepository } from './base.repository'
 import { ModelContainer } from '@decorators/model.decorator'
 import { ItemRepositoryInterface } from './interfaces/item.repository.interface'
 import Item from '@models/entities/items.entity'
+import { WhereOptions } from 'sequelize'
 
 @Service({ global: true })
 class ItemRepository extends BaseRepository<Item> implements ItemRepositoryInterface<Item> {
   constructor(@ModelContainer(Item.tableName) Item: ModelCtor<Item>) {
     super(Item)
+  }
+
+  async getListItems(
+    whereClause: WhereOptions<Item>,
+    offset: number,
+    limit: number,
+    orderBy: any,
+  ): Promise<any> {
+    return this.model.findAndCountAll({
+      where: whereClause,
+      order: orderBy,
+      offset,
+      limit,
+    })
   }
 }
 
