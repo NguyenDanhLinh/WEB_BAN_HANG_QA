7111e1bb3ce87985f25f4e4e07a40a4602864568
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-06 16:08:07 +0700

    create api

--- src/app/controllers/users.controller.ts
+++ src/app/controllers/users.controller.ts
@@ -3,8 +3,10 @@ import { BaseController } from './base.controller'
 import { Service } from 'typedi'
 import UserServices from '@services/users.service'
 import validationMiddleware from '@middlewares/validation.middleware'
-import { CreateUserDto, UserLoginDto } from 'dtos/users.dto'
+import { CreateUserDto, UserLoginDto, ReceiveVoucherDto } from 'dtos/users.dto'
 import { AdminMiddleware } from '@middlewares/checkAdmin.middleware'
+import { RequestWithUser } from '@interfaces/auth.interface'
+import { UserMiddleware } from '@middlewares/checkUser.middleware'
 
 @JsonController('/user')
 @Service()
@@ -35,6 +37,19 @@ export class UsersController extends BaseController {
     const jwt = await this.userServices.userLogin(body)
 
     return this.responseSuccess(jwt, 'Success', res)
+  }
+
+  @Post('/receive-voucher')
+  @UseBefore(UserMiddleware)
+  @UseBefore(validationMiddleware(ReceiveVoucherDto, 'body'))
+  async receiveVoucher(
+    @Body() body: ReceiveVoucherDto,
+    @Req() req: RequestWithUser,
+    @Res() res: any,
+  ) {
+    await this.userServices.receiveVoucher(body.voucherId, req.user.id)
+
+    return this.responseSuccess([], 'Success', res)
   }
 }
 
