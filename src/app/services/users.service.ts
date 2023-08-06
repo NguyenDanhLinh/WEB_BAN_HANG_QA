05ec55204d0b030186c462c8edb6b6ec0aad5d15
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-06 16:07:57 +0700

    create service

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -7,10 +7,19 @@ import jwt from 'jsonwebtoken'
 import { HttpException } from '@exceptions/http.exception'
 import { env } from '@env'
 import CronServices from 'vendor-services/cronJob.service'
+import UserVoucherRepository from '@repositories/user_voucher.repository'
+import VoucherRepository from '@repositories/voucher.repository'
+import DB from '@models/index'
+import { LoggingException } from '@exceptions/index'
 
 @Service()
 class UserServices {
-  constructor(protected userRepository: UserRepository, protected cronServices: CronServices) {}
+  constructor(
+    protected userRepository: UserRepository,
+    protected cronServices: CronServices,
+    protected userVoucherRepository: UserVoucherRepository,
+    protected voucherRepository: VoucherRepository,
+  ) {}
 
   async getAll() {
     return this.userRepository.getAll()
@@ -42,6 +51,30 @@ class UserServices {
       '2023-07-25 08:31:00 +00:00	',
       this.userRepository.getAll(),
     )
+  }
+
+  async receiveVoucher(voucherId: number, userId: number) {
+    const voucher = await this.voucherRepository.findById(voucherId)
+
+    if (voucher.inventoryNumber <= 0) {
+      throw new HttpException(400, 'quantity is out')
+    }
+
+    const transaction = await DB.sequelize.transaction()
+
+    try {
+      await this.voucherRepository.decrement('inventoryNumber', voucherId, 1, transaction)
+
+      await this.userVoucherRepository.create({ voucherId, userId }, transaction)
+
+      transaction.commit()
+
+      return true
+    } catch (error) {
+      transaction.rollback()
+
+      throw new LoggingException(400, error.message, { voucherId, userId })
+    }
   }
 }
 
