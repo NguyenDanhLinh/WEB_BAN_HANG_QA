cf95cc1d0b9af99903260a6839ba1da84b35580d
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-07 11:29:33 +0700

    create repository

--- src/app/repositories/cart.repository.ts
+++ src/app/repositories/cart.repository.ts
@@ -5,6 +5,9 @@ import { ModelContainer } from '@decorators/model.decorator'
 import Cart from '@models/entities/carts.entity'
 import { CartRepositoryInterface } from './interfaces/cart.repository.interface'
 import { Transaction, WhereOptions } from 'sequelize'
+import CartItem from '@models/entities/cart_item.entity'
+import Item from '@models/entities/items.entity'
+import User from '@models/entities/users.entity'
 
 @Service({ global: true })
 class CartRepository extends BaseRepository<Cart> implements CartRepositoryInterface<Cart> {
@@ -23,6 +26,40 @@ class CartRepository extends BaseRepository<Cart> implements CartRepositoryInter
     return this.model.findOrCreate({
       where: whereClause,
       transaction,
+    })
+  }
+
+  async getListItemInCart(
+    whereClause: WhereOptions<Cart>,
+    offset: number,
+    limit: number,
+    orderBy: any,
+  ): Promise<any> {
+    return this.model.findAndCountAll({
+      where: whereClause,
+      include: [
+        {
+          model: CartItem,
+          as: 'cartItem',
+          required: true,
+          include: [
+            {
+              model: Item,
+              as: 'item',
+            },
+          ],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: {
+            exclude: ['createdAt', 'updatedAt','password'],
+          },
+        },
+      ],
+      order: orderBy,
+      offset,
+      limit,
     })
   }
 }
