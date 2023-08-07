bef21e36df193c98c601b9d9b26f1ca316492aaf
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-07 10:57:13 +0700

    create api add item for cart

--- src/app/controllers/cart.controller.ts
+++ src/app/controllers/cart.controller.ts
@@ -5,12 +5,28 @@ import validationMiddleware from '@middlewares/validation.middleware'
 import { CreateUserDto, UserLoginDto } from 'dtos/users.dto'
 import { AdminMiddleware } from '@middlewares/checkAdmin.middleware'
 import CartServices from '@services/cart.service'
+import { UserMiddleware } from '@middlewares/checkUser.middleware'
+import { AddItemToCartDto } from 'dtos/cart.dto'
+import { RequestWithUser } from '@interfaces/auth.interface'
 
 @JsonController('/carts')
 @Service()
 export class CartController extends BaseController {
   constructor(protected cartServices: CartServices) {
     super()
+  }
+
+  @UseBefore(UserMiddleware)
+  @UseBefore(validationMiddleware(AddItemToCartDto, 'body'))
+  @Post('/add-item')
+  async addItemToCart(
+    @Body() body: AddItemToCartDto,
+    @Req() req: RequestWithUser,
+    @Res() res: any,
+  ) {
+    const result = await this.cartServices.addItemToCart(body, req.user.id)
+
+    return this.responseSuccess(result, 'Success', res)
   }
 }
 
