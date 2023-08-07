ee73e7067aa61dd166770ee32f92945fffdb86ef
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-07 10:56:44 +0700

    create function in repository

--- src/app/repositories/cart.repository.ts
+++ src/app/repositories/cart.repository.ts
@@ -4,11 +4,26 @@ import { BaseRepository } from './base.repository'
 import { ModelContainer } from '@decorators/model.decorator'
 import Cart from '@models/entities/carts.entity'
 import { CartRepositoryInterface } from './interfaces/cart.repository.interface'
+import { Transaction, WhereOptions } from 'sequelize'
 
 @Service({ global: true })
 class CartRepository extends BaseRepository<Cart> implements CartRepositoryInterface<Cart> {
   constructor(@ModelContainer(Cart.tableName) Cart: ModelCtor<Cart>) {
     super(Cart)
+  }
+
+  async create(param, transaction?: Transaction): Promise<Cart> {
+    return this.model.create(param, { transaction: transaction })
+  }
+
+  async findOrCreate(
+    whereClause: WhereOptions<Cart>,
+    transaction?: Transaction,
+  ): Promise<[Cart, boolean]> {
+    return this.model.findOrCreate({
+      where: whereClause,
+      transaction,
+    })
   }
 }
 
