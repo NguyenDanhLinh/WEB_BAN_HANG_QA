366c89d1cee6ba42cb2f8386633e3eb03c70cc38
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-09 09:25:51 +0700

    add api create order

--- src/app/controllers/order.controller.ts
+++ src/app/controllers/order.controller.ts
@@ -5,12 +5,24 @@ import validationMiddleware from '@middlewares/validation.middleware'
 import { CreateUserDto, UserLoginDto } from 'dtos/users.dto'
 import { AdminMiddleware } from '@middlewares/checkAdmin.middleware'
 import OrderServices from '@services/order.service'
+import { UserMiddleware } from '@middlewares/checkUser.middleware'
+import { CreateOrderDto } from 'dtos/order.dto'
+import { RequestWithUser } from '@interfaces/auth.interface'
 
 @JsonController('/orders')
 @Service()
 export class OrderController extends BaseController {
   constructor(protected orderServices: OrderServices) {
     super()
+  }
+
+  @UseBefore(UserMiddleware)
+  @UseBefore(validationMiddleware(CreateOrderDto, 'body'))
+  @Post('/create')
+  async createOrder(@Body() body: CreateOrderDto, @Req() req: RequestWithUser, @Res() res: any) {
+    const result = await this.orderServices.createOrder(body, req.user.id)
+
+    return this.responseSuccess(result, 'Success', res)
   }
 }
 
