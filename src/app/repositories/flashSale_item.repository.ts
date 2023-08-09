77191e620b877fe0450900a84f4a3760f0520d01
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-09 09:25:27 +0700

    update repository

--- src/app/repositories/flashSale_item.repository.ts
+++ src/app/repositories/flashSale_item.repository.ts
@@ -4,7 +4,7 @@ import { BaseRepository } from './base.repository'
 import { ModelContainer } from '@decorators/model.decorator'
 import FlashSaleItem from '@models/entities/flashSale_item.entity'
 import { FlashSaleItemRepositoryInterface } from './interfaces/flashSale_item.repository.interface'
-import { Transaction } from 'sequelize'
+import { Transaction, WhereAttributeHashValue } from 'sequelize'
 
 @Service({ global: true })
 class FlashSaleItemRepository
@@ -17,6 +17,25 @@ class FlashSaleItemRepository
 
   async create(param, transaction?: Transaction): Promise<FlashSaleItem> {
     return this.model.create(param, { transaction: transaction })
+  }
+
+  async decrement(
+    field: keyof FlashSaleItem,
+    id: WhereAttributeHashValue<number>,
+    amount: number,
+    transaction?: Transaction,
+  ) {
+    return this.model.decrement(
+      {
+        [field]: amount,
+      },
+      {
+        where: {
+          id,
+        },
+        transaction,
+      },
+    )
   }
 }
 
