e5191355cf75443fb486c6652f3644f9a8933dfd
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-10 15:03:38 +0700

    add api getOrder

--- src/app/controllers/order.controller.ts
+++ src/app/controllers/order.controller.ts
@@ -6,8 +6,12 @@ import { CreateUserDto, UserLoginDto } from 'dtos/users.dto'
 import { AdminMiddleware } from '@middlewares/checkAdmin.middleware'
 import OrderServices from '@services/order.service'
 import { UserMiddleware } from '@middlewares/checkUser.middleware'
-import { CreateOrderDto } from 'dtos/order.dto'
+import { CreateOrderDto, GetOrderDto } from 'dtos/order.dto'
 import { RequestWithUser } from '@interfaces/auth.interface'
+import { GetPagination } from '@decorators/get.pagination.decorator'
+import { Pagination } from '@interfaces/pagination.interface'
+import { Response } from 'express'
+import { PaginationQueryParams } from '@decorators/pagination.query.decorator'
 
 @JsonController('/orders')
 @Service()
@@ -21,6 +25,33 @@ export class OrderController extends BaseController {
   @Post('/create')
   async createOrder(@Body() body: CreateOrderDto, @Req() req: RequestWithUser, @Res() res: any) {
     const result = await this.orderServices.createOrder(body, req.user.id)
+
+    return this.responseSuccess(result, 'Success', res)
+  }
+
+  @UseBefore(AdminMiddleware)
+  @Get('/get')
+  @UseBefore(validationMiddleware(GetOrderDto, 'query'))
+  async getOrder(
+    @GetPagination() pagination: Pagination,
+    @PaginationQueryParams() params: GetOrderDto,
+    @Res() res: Response,
+  ) {
+    const result = await this.orderServices.getOrder(pagination, params)
+
+    return this.responseSuccess(result, 'Success', res)
+  }
+
+  @UseBefore(UserMiddleware)
+  @Get('/get-by-user')
+  @UseBefore(validationMiddleware(GetOrderDto, 'query'))
+  async getOrderByUser(
+    @GetPagination() pagination: Pagination,
+    @PaginationQueryParams() params: GetOrderDto,
+    @Req() req: RequestWithUser,
+    @Res() res: Response,
+  ) {
+    const result = await this.orderServices.getOrder(pagination, params, req.user.id)
 
     return this.responseSuccess(result, 'Success', res)
   }
