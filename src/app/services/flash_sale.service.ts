3fc13140bab25ff058915c285d745b004425f57f
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-10 11:39:35 +0700

    create getFlashSale in service

--- src/app/services/flash_sale.service.ts
+++ src/app/services/flash_sale.service.ts
@@ -8,12 +8,15 @@ import { env } from '@env'
 import CronServices from 'vendor-services/cronJob.service'
 import CategoryRepository from '@repositories/category.repository'
 import FlashSaleRepository from '@repositories/flash_sale.repository'
-import { CreateFlashSaleInterface } from '@interfaces/flashSale.interface'
+import { CreateFlashSaleInterface, GetFlashSaleInterface } from '@interfaces/flashSale.interface'
 import ItemRepository from '@repositories/item.repository'
 import DB from '@models/index'
 import { LoggingException } from '@exceptions/logging.exception'
 import FlashSaleItem from '@models/entities/flashSale_item.entity'
 import FlashSaleItemRepository from '@repositories/flashSale_item.repository'
+import { Pagination } from '@interfaces/pagination.interface'
+import { WhereOptions } from 'sequelize'
+import FlashSale from '@models/entities/flash_sale.entity'
 
 @Service()
 class FlashSaleServices {
@@ -39,33 +42,44 @@ class FlashSaleServices {
     try {
       const flashSale = await this.flashSaleRepository.create(body, transaction)
 
-      await Promise.all(
+      const listCronUpdateSaleItem = await Promise.all(
         items.map((item) => {
           if (item.percent != 0 && item.moneyReduced != '0') {
             throw new HttpException(400, 'percent or moneyReduced must be 0')
           }
 
-          return this.itemRepository.getItems({ id: item.itemId }).then((itemRecord) => {
-            if (
-              parseInt(item.moneyReduced) >= parseInt(itemRecord.outputPrice) ||
-              item.percent >= 100
-            ) {
-              throw new HttpException(400, 'The amount of reduction is too big')
-            }
+          const quantity = item.quantity
 
-            if (itemRecord.flashSaleItem.length > 0) {
-              return
-            }
+          delete item.quantity
 
-            return this.flashSaleItemRepository.create(
-              { flashSaleId: flashSale.id, ...item },
-              transaction,
-            )
-          })
+          return this.itemRepository
+            .getItems({ id: item.itemId })
+            .then((itemRecord) => {
+              if (
+                parseInt(item.moneyReduced) >= parseInt(itemRecord.outputPrice) ||
+                item.percent >= 100
+              ) {
+                throw new HttpException(400, 'The amount of reduction is too big')
+              }
+
+              if (itemRecord.flashSaleItem.length > 0) {
+                return
+              }
+
+              return this.flashSaleItemRepository.create(
+                { flashSaleId: flashSale.id, quantity: 0, ...item },
+                transaction,
+              )
+            })
+            .then((flashSaleItem) => {
+              return { flashSaleId: flashSaleItem.id, quantity: quantity }
+            })
         }),
       )
 
       transaction.commit()
+
+      this.cronServices.createScheduleUpdateFlashSaleItem(body.startDate, listCronUpdateSaleItem)
 
       this.cronServices.createScheduleCloseFlashSale(body.endDate, flashSale.id)
 
@@ -77,6 +91,17 @@ class FlashSaleServices {
 
       throw new LoggingException(400, error.message, { body })
     }
+  }
+
+  async getFlashSale(pagination: Pagination, params: GetFlashSaleInterface) {
+    const { skip, limit, sort, search } = pagination
+
+    const whereClause: WhereOptions<FlashSale> = {
+      ...search,
+      ...params,
+    }
+
+    return this.flashSaleRepository.getFlashSale(whereClause, skip, limit, sort)
   }
 }
 
