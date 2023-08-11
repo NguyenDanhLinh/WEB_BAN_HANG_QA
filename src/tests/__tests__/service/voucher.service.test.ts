a4f46aa4f452a84f3797aa8189cd77fe9b7949cf
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-11 16:23:50 +0700

    unit test for voucher service

---
+++ src/tests/__tests__/service/voucher.service.test.ts
@@ -0,0 +1,93 @@
+import { HttpException } from '@exceptions/http.exception'
+import { CreateVoucherInterface } from '@interfaces/voucher.interface'
+import VoucherServices from '@services/voucher.service'
+import { Container } from 'typedi'
+
+const voucherServices = Container.get(VoucherServices)
+
+describe('voucher.service.ts', () => {
+  describe('#createVoucher()', () => {
+    describe('should return success and expected', () => {
+      const table = [
+        {
+          params: {
+            name: 'giam 2000',
+            percent: 0,
+            moneyReduced: '2000',
+            startDate: new Date('2023-08-09T02:20:00Z'),
+            endDate: new Date('2023-08-09T02:20:00Z'),
+            inventoryNumber: 3,
+          } as CreateVoucherInterface,
+          expected: {
+            name: 'giam 2000',
+            percent: 0,
+            moneyReduced: '2000',
+            startDate: new Date('2023-08-09T02:20:00Z'),
+            endDate: new Date('2023-08-09T02:20:00Z'),
+            inventoryNumber: 3,
+            barcode: 'ABCD1234',
+          },
+          barcode: 'ABCD1234',
+        },
+      ]
+
+      test.each(table)('params: $params.paginations', async ({ params, expected, barcode }) => {
+        const generateRandomBarcodeSpy = jest
+          .spyOn(voucherServices, 'generateRandomBarcode')
+          .mockReturnValueOnce(barcode)
+
+        const actualReturn = await voucherServices.createVoucher(params)
+
+        expect(generateRandomBarcodeSpy).toHaveBeenCalled()
+
+        generateRandomBarcodeSpy.mockRestore()
+
+        expect(actualReturn).toMatchObject(expected)
+      })
+    })
+
+    describe('percent > 100, should throw HttpException The amount of reduction is too big', () => {
+      const table = [
+        {
+          params: {
+            name: 'giam 105%',
+            percent: 105,
+            moneyReduced: '0',
+            startDate: new Date('2023-08-09T02:20:00Z'),
+            endDate: new Date('2023-08-09T02:20:00Z'),
+            inventoryNumber: 3,
+          } as CreateVoucherInterface,
+          expected: new HttpException(400, 'The amount of reduction is too big'),
+        },
+      ]
+
+      test.each(table)('params: $params.paginations', async ({ params, expected }) => {
+        await expect(voucherServices.createVoucher(params)).rejects.toThrow(expected)
+      })
+    })
+
+    describe('#generateRandomBarcode()', () => {
+      describe('should return success and expected', () => {
+        const table = [
+          {
+            expected: {
+              name: 'giam 2000',
+              percent: 0,
+              moneyReduced: '2000',
+              startDate: new Date('2023-08-09T02:20:00Z'),
+              endDate: new Date('2023-08-09T02:20:00Z'),
+              inventoryNumber: 3,
+              barcode: 'ABCD1234',
+            },
+          },
+        ]
+
+        test.each(table)('params: $params.paginations', async ({ expected }) => {
+          const actualReturn = await voucherServices.generateRandomBarcode()
+
+          expect(typeof actualReturn).toBe('string')
+        })
+      })
+    })
+  })
+})
