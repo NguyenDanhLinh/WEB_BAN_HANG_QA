a32ff4fe471f69a2054635f07618c3edf9e7571d
NguyenDanhLinh <linhtru2001@gmail.com>
2023-08-11 10:33:23 +0700

    create dto, interface

--- src/interfaces/cart.interface.ts
+++ src/interfaces/cart.interface.ts
@@ -2,3 +2,11 @@ export interface AddItemToCart {
   itemId: number
   quantity: number
 }
+
+export interface DeleteItemToCart {
+  itemId: number
+}
+
+export interface IncrementItemToCart extends DeleteItemToCart {
+  quantity?: number
+}
